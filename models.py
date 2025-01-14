94ecf8e49d86e4a108324abc0bcc9b415834a8b0
ThanhHienCD <tranthanhhien040205@gmail.com>
2025-01-14 21:41:02 +0700

    update model.py Blog

--- models.py
+++ models.py
@@ -1,137 +1,67 @@
-
 from django.db import models
-from datetime import datetime
-from django.urls import reverse
-from django.utils import timezone
-from django.contrib.auth import get_user_model
-User=get_user_model()
+
 # Create your models here.
-class Category(models.Model):
-    name=models.CharField(unique=True,max_length=200,db_index=True)
-    slug=models.SlugField(max_length=200,unique=True)
-    
-    def get_absolute_url(self):
-        return reverse('auction:items_by_category',args=[self.slug])
-    
-    class Meta:
-        ordering=('name',)
-        verbose_name='category'
-        verbose_name_plural='categories'
-    
-    def __str__(self):
-        return self.name 
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
-class Seller(models.Model):
-    name=models.CharField(max_length=200)
-    seller_photo=models.ImageField(upload_to='photos/seller/%Y/%m/%d/')
-    contact_no =models.CharField(max_length=200)
-    email=models.CharField(max_length=200)
-    def get_absolute_url(self):
-        return reverse('auction:seller-page',args=[self.id])
     
-    def __str__(self):
-        return self.name
+class PublishedManager(models.Manager):
     
-class Lot(models.Model):
-    category=models.ForeignKey(Category, related_name='lots', on_delete=models.CASCADE)
-    seller =models.ForeignKey(Seller,on_delete=models.DO_NOTHING)
-    slug=models.SlugField(max_length=200,db_index=True)
-    product_name=models.CharField(max_length=200,db_index=True)
-    is_live=models.BooleanField(default=False)
-    base_price=models.IntegerField(default=0.0)
-    current_price=models.IntegerField(default=0.0)
-    description=models.CharField(max_length=500)
-    main_photo=models.ImageField(upload_to='photos/main/%Y/%m/%d/')
-    photo1=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
-    photo2=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
-    photo3=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
-    photo4=models.ImageField(upload_to='photos/optional/%Y/%m/%d/',blank=True)
-    is_trending=models.BooleanField(default=False)
-    on_banner=models.BooleanField(default=False)
-    year_published=models.DateTimeField(default=datetime.now())
-    #  tags:one to many fields 
-    
-    def get_absolute_url(self):
-        return reverse('auction:single-item',args=[self.id,self.slug])
-    
-    
-    
-    
-    
-    class Meta:
-        ordering = ('product_name',)  # Sắp xếp theo product_name
-        indexes = [
-            models.Index(fields=['id', 'slug']),  
-        ]
-    
-    
-    def __str__(self):
-        return self.product_name
-    
-  
-class Wishlist(models.Model):
-    lot=models.CharField(max_length=1000)
-    name=models.CharField(max_length=200)
-    slug=models.SlugField(max_length=200,db_index=True,null=True,blank=True)
-    lot_id=models.IntegerField()
-    Wishlisted_date=models.DateTimeField(default=datetime.now,blank=True)
-    user_id=models.IntegerField(blank=False)
-    
-    def get_absolute_url(self):
-        return reverse('auction:single-item',args=[self.lot_id,self.slug])
-    def __srt__(self):
-        return self.name
-
-class Contact(models.Model):
-    lot=models.CharField(max_length=1000)
-    lot_id=models.IntegerField()
-    name=models.CharField(max_length=200)
-    email=models.CharField(max_length=200)
-    slug=models.SlugField(max_length=200,db_index=True,null=True,blank=True)
-    message=models.TextField(blank=True)
-    contact_date=models.DateTimeField(default=datetime.now,blank=True)
-    user_id=models.IntegerField(blank=True)
-    
-    def get_absolute_url(self):
-        return reverse('auction:single-item',args=[self.lot_id,self.slug])
-    
-    
-    
-    def __str__(self):
-        return self.name
-    
-    
-  
-class Auction(models.Model):
-    start =  models.DateTimeField(auto_now=False)
-    curr_time =  models.DateTimeField(auto_now=False)
-    item = models.OneToOneField(
-    Lot, on_delete=models.CASCADE, related_name="auction")
-    
-    
-    def __str__(self):
-        return self.item.product_name
-
-
-
-class Message(models.Model):
-    auction=models.ForeignKey(Auction, on_delete=models.CASCADE,related_name='auction') 
-    author=models.ForeignKey(User,related_name='author_messsages',on_delete=models.CASCADE)
-    timecap = models.DateTimeField(auto_now_add=True)
-    price = models.TextField()
-   
-    
-    
-
-    def __str__(self):
-        return self.author.username
-    
-    
-    
-class Subscribe(models.Model):
-    email=models.EmailField()
-    
-    def __str__(self):
-        return self.email
-  
+    def get_queryset(self):
         
+        return super(PublishedManager,
+                   self).get_queryset().filter(status='published')
+       
+
+
+class Post(models.Model):
+    STATUS_CHOICES=(
+        ('draft','Draft'),
+        ('published','Published')
+    )
+    
+    title=models.CharField(max_length=300)
+    slug=models.SlugField(max_length=300,unique_for_date='publish')
+    author=models.ForeignKey(User,related_name='blog_posts',on_delete=models.CASCADE)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(default=timezone.now)
+    updated=models.DateTimeField(default=timezone.now)
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects=models.Manager()
+    published=PublishedManager()
+    
+    tags=TaggableManager()  #it allows to add,retrive and remove tags from post.
+    
+    class Meta:
+        ordering=('-publish',)  
+        #ERRORS:
+        #blog.Post: (models.E014) 'ordering' must be a tuple or list (even if you want to order by only one field).
+    
+    def get_absolute_url(self):
+        return reverse('blog-app:post-detail', 
+                       args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug]
+                       )
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name=models.CharField(max_length=100)
+    email=models.EmailField()
+    body=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+    active=models.BooleanField(default=True)
+    
+    class Meta:
+        ordering=('created',)
+        
+    def __str__(self):
+        return f" Comments by {self.name} on {self.post}"
+            
+            
+    
